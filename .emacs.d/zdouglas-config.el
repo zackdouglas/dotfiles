3855a627701420759f3dfd7a1ca01cc83c8da6f8
Zack Douglas <zackery.douglas@gmail.com>
2012-08-05 17:34:31 -0300

    backup to homedir, not sourcedir

--- .emacs.d/zdouglas-config.el
+++ .emacs.d/zdouglas-config.el
@@ -6,3 +6,19 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 (setq indent-line-function 'insert-tab)
+
+;; from <https://raw.github.com/docwhat/homedir-examples/master/emacs-base/.emacs.d/common/backups.el>
+;; Set all files to backup in ~/emacs.d/backups instead of
+;; in the same directory
+(setq-default backup-directory-alist 
+              (list 
+               (cons ".*" ; a regex, not glob
+                     (expand-file-name "~/.emacsbackup/")
+                     )
+               ))
+
+;; Preserve hard links when editing
+(setq-default backup-by-copying-when-linked t)
+
+;; Preserve owner and group of the file you're editing
+(setq-default backup-by-copying-when-mismatch t)
